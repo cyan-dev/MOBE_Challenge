86af6bd1645de3be2a0a35e15a6cd96c1da7f05f
Cyril Esclassan <cyril.esclassan@outlook.com>
2023-03-17 09:13:42 +0100

    Init Android project LSD(L)

---
+++ app/src/main/java/fr/ut3/sdl/mobe/saucisse/lsdl/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.ut3.sdl.mobe.saucisse.lsdl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
