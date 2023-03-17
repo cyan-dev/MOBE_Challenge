ba6b3c06381040ba77e9d593b1aa88d70f818a8e
AZIZEN Florian <florian.azizen@master-developpement-logiciel.fr>
2023-03-17 09:54:30 +0100

    Creation of the labyrinth game and configuration of the project

---
+++ app/src/main/java/fr/ut3/sdl/mobe/saucisse/lsdl/LabyrinthGame.java
@@ -0,0 +1,14 @@
+package fr.ut3.sdl.mobe.saucisse.lsdl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LabyrinthGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labyrinth_game);
+    }
+}
