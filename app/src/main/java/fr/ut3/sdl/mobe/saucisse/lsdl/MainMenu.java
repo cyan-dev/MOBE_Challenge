d185e5e8f305b5542b44dab9c0fae0bb53502b4e
Dylan Caron <dylan.caron@master-developpement-logiciel.fr>
2023-03-17 11:47:21 +0100

    Labyrinth

--- app/src/main/java/fr/ut3/sdl/mobe/saucisse/lsdl/MainMenu.java
+++ app/src/main/java/fr/ut3/sdl/mobe/saucisse/lsdl/MainMenu.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import fr.ut3.sdl.mobe.saucisse.lsdl.parts.Labyrinth;
+
 public class MainMenu extends AppCompatActivity {
     public void startMedicActivity(View view){
         Intent intent = new Intent(this, MedicGame.class);
@@ -22,5 +24,7 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        System.out.println(new Labyrinth().transform(10, 10, 20, 5));
     }
 }
